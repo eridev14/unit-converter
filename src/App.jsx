ba5b1ca0da110359973b0ee0d4e29e3014331b5d
eridev14 <eridev14hc@gmail.com>
2024-07-02 19:36:49 -0400

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,22 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronDown,
-  faGear,
-  faTemperatureThreeQuarters,
-} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
 
 import "./App.css";
+import { Header } from "./Header";
+import { BtnConvertion } from "./BtnConvertion";
+import { useState } from "react";
 
 function App() {
+  const [convert, setConvert] = useState("temperatura");
   return (
     <div className="content">
-      <header className="header">
-        <nav className="nav">
-          <h1 className="title">Unit Converter</h1>
-          <button className="btn btn--nav">
-            <FontAwesomeIcon icon={faGear} />
-          </button>
-        </nav>
-        <div className="convert ">
-          <div className="convert__side convert__side--from">
-            <label htmlFor="txtFrom" className="convert__label">
-              Celsius
-            </label>
-            <input
-              type="number"
-              name="txtFrom"
-              id="txtFrom"
-              className="convert__input"
-            />
-            <section className="checked">
-              <div className="checked__item">
-                <input type="checkbox" className="checked__check" />
-                <label className="checked__label">Celsuis</label>
-              </div>
-              <div className="checked__item">
-                <input type="checkbox" className="checked__check" />
-                <label className="checked__label">Celsuis</label>
-              </div>
-            </section>
-          </div>
-          <div className="convert__side convert__side--to">
-            <label htmlFor="txtFrom" className="convert__label">
-              Celsius
-            </label>
-            <input
-              type="number"
-              name="txtFrom"
-              id="txtFrom"
-              className="convert__input"
-            />
-            <section className="checked">
-              <div className="checked__item">
-                <input type="checkbox" className="checked__check" />
-                <label className="checked__label">Celsuis</label>
-              </div>
-              <div className="checked__item">
-                <input type="checkbox" className="checked__check" />
-                <label className="checked__label">Celsuis</label>
-              </div>
-            </section>
-          </div>
-          <span className="convert__equal">=</span>
-          <button className="btn btn--up">
-            <FontAwesomeIcon icon={faChevronDown} />
-          </button>
-        </div>
-      </header>
-
+      <Header />
       <section className="convertions">
-        <h2 className="convertions__title">Convertions</h2>
+        <h2 className="convertions__title">Convertions: Temperatura</h2>
         <div className="convertions__grid">
-          <button className="btn btn--convertions">
-            <FontAwesomeIcon
-              icon={faTemperatureThreeQuarters}
-              className="btn__icon"
-            />
-            <span className="btn__text">Temp</span>
-          </button>
-          <button className="btn btn--convertions">
-            <FontAwesomeIcon
-              icon={faTemperatureThreeQuarters}
-              className="btn__icon"
-            />
-            <span className="btn__text">Temp</span>
-          </button>
-          <button className="btn btn--convertions">
-            <FontAwesomeIcon
-              icon={faTemperatureThreeQuarters}
-              className="btn__icon"
-            />
-            <span className="btn__text">Temp</span>
-          </button>
-          <button className="btn btn--convertions">
-            <FontAwesomeIcon
-              icon={faTemperatureThreeQuarters}
-              className="btn__icon"
-            />
-            <span className="btn__text">Temp</span>
-          </button>
+          <BtnConvertion />
         </div>
       </section>
     </div>
